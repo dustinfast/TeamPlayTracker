e7c72dbeef42bd8ad43bf3026f16245545c57ad9
woodsBrooks <31783147+woodsBrooks@users.noreply.github.com>
2017-10-25 17:52:39 -0800

    This better work - BW

--- AWGAEventTracker/ManageEvents1.cs
+++ AWGAEventTracker/ManageEvents1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.OleDb;
 using System.Text.RegularExpressions;
 //test comment
+//test comment 3
 namespace AWGAEventTracker
 {
     public partial class ManageEvents1 : Form
